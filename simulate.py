9316a923512a58c16cfcd3af4c6caef751e42b7e
kat-myers <kathrynmyers2027@u.northwestern.edu>
2023-01-20 00:43:09 -0600

    refactoring, functioning!

---
+++ simulate.py
@@ -0,0 +1,13 @@
+import pybullet as p
+import pybullet_data
+import time
+import pyrosim.pyrosim as pyrosim
+import numpy as np
+import random
+import constants as c
+from simulation import SIMULATION
+
+simulation = SIMULATION()
+simulation.Run()
+
+
